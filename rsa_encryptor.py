a7f27900dd5c099be7480b01c177610d6a768340
zachbook <zekar1zekarasferie@gmail.com>
2023-12-28 15:01:57 -0500

    return file path instead of file object. needs rework

--- rsa_encryptor.py
+++ rsa_encryptor.py
@@ -15,23 +15,21 @@ class RSA_encryptor():
                 ciphered_byte = int(math.pow(byte,e) % public_keyn)
                 output_file.write(int.to_bytes(ciphered_byte))
         output_file.close()
-        return output_file
+        return "encrypted.txt"
     
     def decrypt(self,d,N,file):
         #the decrypted file to be outputed
-        output_file = open("output.txt","+wb")
+        output_file = "output.txt";
 
         with open(file,"rb") as file:
             data = file.read()
             for byte in data:
                 deciphered_byte =pow(byte,d,N)        
-                output_file.write(int.to_bytes(deciphered_byte))
+                output_file.write(int.to_bytes(deciphered_byte, 4, 'big'))
         print("File successfully decrypted")
-        output_file.close()
+        
         return output_file
 
 
-en = RSA_encryptor()
-en.encrypt(187,3,"sample.txt")
-#en.decrypt(107,187,"dec.png")
+
         
